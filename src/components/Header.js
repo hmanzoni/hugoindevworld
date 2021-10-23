36a48e7acf040c96b639a3048aa584495b65eeee
hugo cm <hugocmdesign@gmail.com>
2021-10-24 01:04:04 +0200

    Add fn for switch theme to dark light

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,16 @@
-import React, {useState} from 'react';
-import { UilTimes, UilMoon, UilApps, UilEstate, UilUser, UilFileAlt, UilBriefcaseAlt, UilScenery, UilMessage } from '@iconscout/react-unicons'
+import React, {useState, useEffect} from 'react';
+import {
+  UilTimes,
+  UilMoon,
+  UilApps,
+  UilEstate,
+  UilUser,
+  UilFileAlt,
+  UilBriefcaseAlt,
+  // UilScenery,
+  // UilMessage,
+  UilSun 
+} from '@iconscout/react-unicons';
 
 const NavItems = ({ infoCard, hideMenu }) => (
   <li className="nav__item">
@@ -11,19 +22,33 @@ const NavItems = ({ infoCard, hideMenu }) => (
 );
 
 const Header = () => {
+  const darkTheme = 'dark-theme';
   const [isActive, setActive] = useState(false);
+  const [darkThemeActive, setDarkThemeActive] = useState(false);
+
+  const getCurrentTheme = (selectTheme) => selectTheme !== 'dark' ? 'dark' : 'light';
+
+  useEffect(() => {
+    const selectTheme = localStorage.getItem('selected-theme');
+    localStorage.setItem('selected-theme', getCurrentTheme(selectTheme));
+    if (darkThemeActive) {
+      document.body.classList.add(darkTheme);
+    } else {
+      document.body.classList.remove(darkTheme);
+    }
+  }, [darkThemeActive]);
 
   const linksNavitems = [
     { link: '#home', title: 'Home', icon: <UilEstate className="nav__icon"/> },
     { link: '#about', title: 'About', icon: <UilUser className="nav__icon"/> },
     { link: '#skills', title: 'Skills', icon: <UilFileAlt className="nav__icon"/> },
     { link: '#services', title: 'Services', icon: <UilBriefcaseAlt className="nav__icon"/> },
-    { link: '#portfolio', title: 'Portfolio', icon: <UilScenery className="nav__icon"/> },
+    // { link: '#portfolio', title: 'Portfolio', icon: <UilScenery className="nav__icon"/> },
     // { link: '#contactme', title: 'Contactme', icon: <UilMessage className="nav__icon"/> },
   ];
 
   return (
-    <header className="header" id="header">
+    <header className="header scroll-header" id="header">
       <nav className="nav container">
         <a href="!#" className="nav__logo">
           Hugo
@@ -34,11 +59,16 @@ const Header = () => {
               <NavItems key={index} infoCard={linkNavitem} hideMenu={() => setActive(false)} />
             ))}
           </ul>
-          <UilTimes onClick={ () => setActive(false)} className="nav__close" id="nav-close" />
+          <UilTimes onClick={() => setActive(false)} className="nav__close" id="nav-close" />
         </div>
 
         <div className="nav__btns">
-          <UilMoon className="change-theme" id="theme-button" />
+            {
+              darkThemeActive ?
+              <UilSun className="change-theme" id="theme-button" onClick={() => setDarkThemeActive(false)} />
+              :
+              <UilMoon className="change-theme" id="theme-button" onClick={() => setDarkThemeActive(true)} />
+            }
           <div className="nav__toggle" id="nav-toggle">
             <UilApps onClick={ () => setActive(true)} />
           </div>
