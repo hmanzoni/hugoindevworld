08d4e9a20331ad5353b8e0baea0421ba14476265
hugo cm <hugocmdesign@gmail.com>
2021-10-23 16:18:18 +0200

    Add a qualification comp for show tech info

---
+++ src/components/Qualification.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import { UilCalendarAlt, UilGraduationCap, UilBriefcaseAlt } from '@iconscout/react-unicons'
+
+const SingleQualification = ({ infoCard }) => (
+  <div className="qualification__data">
+    {!infoCard.isRight && (
+      <>
+        <div></div>
+        <div>
+          <span className="qualification__rounder"></span>
+          <span className="qualification__line"></span>
+        </div>
+      </>
+    )}
+    <div>
+      <h3 className="qualification__title">{infoCard.title}</h3>
+      <span className="qualification__subtitle">{infoCard.subtitle}</span>
+      <div className="qualification__calendar">
+        <UilCalendarAlt />
+        {infoCard.rangeYears}
+      </div>
+    </div>
+    {infoCard.isRight && (
+      <div>
+        <span className="qualification__rounder"></span>
+        <span className="qualification__line"></span>
+      </div>
+    )}
+  </div>
+);
+
+const Qualification = () => {
+  const educations = [
+    {
+      id: 1,
+      title: 'Computer Enginenr',
+      subtitle: 'Arg - University',
+      rangeYears: '2009 - 2012',
+      isRight: true,
+    },
+    {
+      id: 2,
+      title: 'Web Design',
+      subtitle: 'Arg - University',
+      rangeYears: '2011 - 2014',
+      isRight: false,
+    },
+    {
+      id: 3,
+      title: 'Web Develop',
+      subtitle: 'Arg - University',
+      rangeYears: '2016 - 2017',
+      isRight: true,
+    },
+    {
+      id: 4,
+      title: 'Master in UI/UX',
+      subtitle: 'Udemy',
+      rangeYears: '2018 - 2020',
+      isRight: false,
+    },
+  ];
+  const works = [
+    {
+      id: 1,
+      title: 'Software Enginenr',
+      subtitle: 'Microsoft',
+      rangeYears: '2009 - 2012',
+      isRight: true,
+    },
+    {
+      id: 2,
+      title: 'Frontend Developer',
+      subtitle: 'Apple',
+      rangeYears: '2012 - 2014',
+      isRight: false,
+    },
+    {
+      id: 3,
+      title: 'Ui Designer',
+      subtitle: 'Figma - Spain',
+      rangeYears: '2014 - 2016',
+      isRight: true,
+    },
+    {
+      id: 4,
+      title: 'Freelancer',
+      subtitle: 'Italy',
+      rangeYears: '2016 - ',
+      isRight: false,
+    },
+  ];
+
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My personal journey</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className="qualification__button button--flex qualification__active"
+            data-target="#education"
+          >
+            <UilGraduationCap className="qualification__icon" />
+            Education
+          </div>
+          <div
+            className="qualification__button button--flex"
+            data-target="#work"
+          >
+            <UilBriefcaseAlt className="qualification__icon" />
+            Work
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div
+            className="qualification__content qualification__active"
+            data-content
+            id="education"
+          >
+            {educations.map((education) => (
+              <SingleQualification key={education.id} infoCard={education} />
+            ))}
+          </div>
+          <div className="qualification__content" data-content id="work">
+            {works.map((work) => (
+              <SingleQualification key={work.id} infoCard={work} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
