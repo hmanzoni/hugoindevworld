b7aabaefe42ebc9d6120d8ef4f16295d83daed81
hugo cm <hugocmdesign@gmail.com>
2021-10-23 19:27:37 +0200

    add click handler for close

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { UilCheckCircle, UilBrushAlt, UilWebGrid, UilServer, UilArrowRight, UilTimes } from '@iconscout/react-unicons'
 
 const ServicesList = ({ text }) => (
@@ -8,7 +8,7 @@ const ServicesList = ({ text }) => (
   </li>
 );
 
-const ServicesContent = ({ cardInfo }) => {
+const ServicesContent = ({ cardInfo, clickerFn }) => {
   return (
     <div className="services__content">
       <div>
@@ -19,20 +19,20 @@ const ServicesContent = ({ cardInfo }) => {
           {cardInfo.subtitle}
         </h3>
       </div>
-      <span className="button button--flex button--small button--link services__button">
+      <span className="button button--flex button--small button--link services__button" onClick={() => clickerFn(cardInfo.code)}>
         View More
         <UilArrowRight className="button__icon" />
       </span>
-      <div className="services__modal">
+      <div className={`services__modal ${cardInfo.isOpen && 'active-modal'}`} onClick={(e) => e.target.className === 'services__modal active-modal' && clickerFn(cardInfo.code)}>
         <div className="services__modal-content">
           <h4 className="services__modal-title">
             {cardInfo.title}
             <br />
             {cardInfo.subtitle}
           </h4>
-          <UilTimes className="services__modal-close" />
+          <UilTimes className="services__modal-close" onClick={() => clickerFn(cardInfo.code)} />
           <ul className="services__modal-services grid">
-            {cardInfo.texts.map((text, index) => (
+            {cardInfo.textsList.map((text, index) => (
               <ServicesList key={index} text={text} />
             ))}
           </ul>
@@ -43,12 +43,15 @@ const ServicesContent = ({ cardInfo }) => {
 };
 
 const Services = () => {
-  const servicesContent = [
+  const [servicesContent, setServicesContent] = useState([])
+  const services = [
     {
       title: 'Ui/Ux',
       icon: <UilBrushAlt className="services__icon" />,
-      subtitle: 'Designer',
-      texts: [
+      subtitle: 'Ui/Ux',
+      code: 'UX',
+      isOpen: false,
+      textsList: [
         'I develop the user interface.',
         'Web page development.',
         'I create ux element interaction.',
@@ -58,8 +61,10 @@ const Services = () => {
     {
       title: 'Frontend',
       icon: <UilWebGrid className="services__icon" />,
-      subtitle: 'Designer',
-      texts: [
+      subtitle: 'Frontend',
+      code: 'FE',
+      isOpen: false,
+      textsList: [
         'I develop the user interface.',
         'Web page development.',
         'I create ux element interaction.',
@@ -69,8 +74,10 @@ const Services = () => {
     {
       title: 'Backend',
       icon: <UilServer className="services__icon" />,
-      subtitle: 'Designer',
-      texts: [
+      subtitle: 'Backend',
+      code: 'BE',
+      isOpen: false,
+      textsList: [
         'I develop the user interface.',
         'Web page development.',
         'I create ux element interaction.',
@@ -78,13 +85,29 @@ const Services = () => {
       ],
     },
   ];
+
+  if (!servicesContent.length) {
+    setServicesContent(services);
+  }
+
+  const handlerOpenClose = (servCode) => {
+    const index = servicesContent.findIndex(skill => skill.code === servCode);
+    const changeState = !servicesContent[index].isOpen;
+    setServicesContent([
+         ...servicesContent.slice(0, index),
+        { ...servicesContent[index], isOpen: changeState },  
+         ...servicesContent.slice(index + 1)
+      ]
+    );
+  }
+
   return (
     <section className="services section">
       <h2 className="section__title">Services</h2>
       <span className="section__subtitle">What i offer</span>
       <div className="services__container container grid">
         {servicesContent.map((serviceContent, index) => (
-          <ServicesContent key={index} cardInfo={serviceContent} />
+          <ServicesContent key={index} cardInfo={serviceContent} clickerFn={handlerOpenClose} />
         ))}
       </div>
     </section>
