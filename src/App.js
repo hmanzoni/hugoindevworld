c891a8fbc4b7d716d6bb1906324647aeefe65c38
hugo cm <hugocmdesign@gmail.com>
2022-01-05 21:35:01 +0100

    Add the context to my components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Hugo from './components/Hugo';
+import IconsState from './context/icons/iconsState';
 
 function App() {
   return (
-    <>
+    <IconsState>
       <Hugo />
-    </>
+    </IconsState>
   );
 }
 
