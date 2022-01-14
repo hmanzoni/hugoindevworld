9a7695cb41befa384688df7e108c07f49ca3c9e1
hugo cm <hugocmdesign@gmail.com>
2022-01-14 08:54:22 +0100

    Create a funct for search the icons

--- src/components/ui/icons.js
+++ src/components/ui/icons.js
@@ -36,3 +36,18 @@ export const arrIcons = [
   {iconName: 'UilDribbble', iconSize: sizeIcons},
   {iconName: 'UilGithubAlt', iconSize: sizeIcons},
 ];
+
+export const foundIcon = (icons, nameIcon, classIcon = '', sizeIcon = '') => icons.find(i => {
+  const checkName = i.type.name === nameIcon;
+  const checkClass = i.props.className === classIcon;
+  const checkSize = i.props.size === sizeIcon;
+  if (classIcon && sizeIcon) {
+    return checkName && checkClass && checkSize;
+  } else if (classIcon) {
+    return checkName && checkClass;
+  } else if (sizeIcon) {
+    return checkName && checkSize;
+  } else {
+    return checkName;
+  }
+});
