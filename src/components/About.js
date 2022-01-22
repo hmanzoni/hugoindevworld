70722a32f027721cebec21f4772ae7ba9b899707
hugo cm <hugocmdesign@gmail.com>
2022-01-22 02:06:29 +0100

    Adapt about page with JSON info

--- src/components/About.js
+++ src/components/About.js
@@ -1,62 +1,58 @@
 import React, { useContext } from 'react';
 import '../assets/css/about.css';
 import about from '../assets/img/about.jpg';
-import cv from '../assets/pdf/Alexa-Cv.pdf';
+import hugoCV from '../assets/pdf/HugoManzoni.CV.pdf';
 import iconContext from '../context/icons/iconsContext';
 import { foundIcon } from './ui/icons';
+import aboutInfo from './data/about';
 
 const SingleAboutCard = ({ infoCard }) => {
+  let title = infoCard.title;
+  const {name, desc} = infoCard;
+  if (name === 'Years') {
+    const yearsWorked = (new Date().getUTCFullYear() - title).toString();
+    title = yearsWorked.length === 1 ? `0${yearsWorked}` : yearsWorked;
+  }
   return (
     <div>
-      <span className="about__info-title">{infoCard.title}+</span>
+      <span className="about__info-title">{title}+</span>
       <span className="about__info-name">
-        {infoCard.name}
+        {name}
         <br />
-        {infoCard.desc}
+        {desc}
       </span>
     </div>
   );
 };
 
+const TextDesc = ({text}) => <>{text}<br /></>;
+
 const About = () => {
   const iconsContext = useContext(iconContext);
   const { icons } = iconsContext;
   
-  const downloadAlt = foundIcon(icons, 'UilDownloadAlt');
-  const yearsWorked = (new Date().getUTCFullYear() - '2018').toString();
+  const {title, subtitle, downloadAltIcon, description, aboutCardsInfo, downloadText} = aboutInfo;
+  const downloadAlt = foundIcon(icons, downloadAltIcon);
 
-  const aboutCardsInfo = [
-    { title: yearsWorked, name: 'Years', desc: 'experience' },
-    { title: '20', name: 'Completed', desc: 'project' },
-    { title: '05', name: 'Companies', desc: 'worked' },
-  ];
   return (
     <section className="about section" id="about">
-      <h2 className="section__title">About me</h2>
-      <span className="section__subtitle">How I start in IT world</span>
+      <h2 className="section__title">{title}</h2>
+      <span className="section__subtitle">{subtitle}</span>
 
       <div className="about__container container grid">
         <img src={about} alt="about" className="about__img" />
         <div className="about__data">
           <p className="about__description">
-            I have been a software enthusiast IT since 2015.
-            <br />
-            As a professional, my main goal is to increase my knowledge in this field, things like new technologies and programming languages. In particular, I look enjoy discovering their weaknesses and strengths.
-            <br />
-            In addition, I consider myself a self-motivated, enthusiastic and resilient person and I have the ability to learn fast and adapt to any changes quickly. Moreover, I am very sociable, I love to learn from other cultures.
-            <br />
-            I have a degree on biochemistry and worked in this field for some years. Nevertheless, in 2015 I started learning about IT, and finally I pursued my dream of starting with an independent team for websites for clients for a living.
+            {description.map((singleText, index) => <TextDesc key={index} text={singleText}></TextDesc>)}
           </p>
 
           <div className="about__info">
-            {aboutCardsInfo.map((infoCard, index) => (
-              <SingleAboutCard key={index} infoCard={infoCard} />
-            ))}
+            {aboutCardsInfo.map((infoCard) => <SingleAboutCard key={infoCard.id} infoCard={infoCard} />)}
           </div>
 
           <div className="about__buttons">
-            <a download="" href={cv} className="button button--flex">
-              Download CV
+            <a download="" href={hugoCV} className="button button--flex">
+              {downloadText}
               {downloadAlt}
             </a>
           </div>
