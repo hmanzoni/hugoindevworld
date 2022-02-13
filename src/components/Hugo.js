696bd2b8188aa917e64171c365539be8693818fa
hugo cm <hugocmdesign@gmail.com>
2022-02-13 02:42:02 +0100

    cancel loading controller of context

--- src/components/Hugo.js
+++ src/components/Hugo.js
@@ -1,50 +1,36 @@
 import React, { useContext, useEffect } from 'react';
+import { arrIcons, foundIcon } from './ui/icons';
 import iconContext from '../context/icons/iconsContext';
 import About from './About';
-// import Portfolio from './Portfolio';
-// import Banner from './Banner';
-// import Testimonial from './Testimonial';
-// import ContactMe from './ContactMe';
-import Footer from './Footer';
-import Header from './Header';
 import Home from './Home';
-import Loader from './Loader';
-import Qualification from './Qualification';
-import Services from './Services';
+import Header from './Header';
 import Skills from './Skills';
-import { arrIcons, foundIcon } from './ui/icons';
+import Services from './Services';
+import Qualification from './Qualification';
+import Footer from './Footer';
 
 const Hugo = () => {
   const iconsContext = useContext(iconContext);
-  const { loadingFinish, loading, getIcons, icons } = iconsContext;
+  const { getIcons, icons } = iconsContext;
 
   useEffect(() => {
     if (!icons.length) {
       getIcons(arrIcons);
     }
-    if (loading) {
-      loadingFinish();
-    }
-  }, [loading, icons, getIcons, loadingFinish]);
+  }, [, icons, getIcons ]);
 
   const arrowUp = foundIcon(icons, 'UilArrowUp', 'scrollup__icon');
 
   return (
     <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          <Header />
-          <Home />
-          <About />
-          <Skills />
-          <Qualification />
-          <Services />
-          <Footer />
-          <a href="!#" className="show-scroll scrollup" id="scroll-up">{arrowUp}</a>
-        </>
-      )}
+      <Header />
+      <Home />
+      <About />
+      <Skills />
+      <Qualification />
+      <Services />
+      <Footer />
+      <a href="!#" className="show-scroll scrollup" id="scroll-up">{arrowUp}</a>
     </>
   );
 };
