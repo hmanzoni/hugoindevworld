1d1e760ad9d97d6c2337db7d855887e2143fae00
hugo cm <hugocmdesign@gmail.com>
2022-02-19 17:57:08 +0100

    handle the header information with json

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import {
 import '../assets/css/header.css';
 import customContext from '../context/customs/customsContext';
 import foundIcon from './functions/foundIcon';
+import headerInfo from './data/header';
 
 const NavItems = ({ infoCard, hideMenu }) => (
   <li className="nav__item">
@@ -17,18 +18,20 @@ const NavItems = ({ infoCard, hideMenu }) => (
 
 const Header = () => {
   const darkTheme = 'dark-theme';
-  const langAvailables = ['EN', 'ES', 'IT'];
+  const itemNameStorage = 'selected-theme';
+  const { name, linkName, defaultTheme, availablesLang } = headerInfo['unique'];
   const [isActive, setActive] = useState(false);
-  const [darkThemeActive, setDarkThemeActive] = useState(false);
+  const [darkThemeActive, setDarkThemeActive] = useState(defaultTheme);
 
   const customsContext = useContext(customContext);
   const { icons, changeLanguage, language } = customsContext;
+  const {linksNavitems} = headerInfo[language || 'en'];
 
   const getCurrentTheme = (selectTheme) => selectTheme !== 'dark' ? 'dark' : 'light';
 
   useEffect(() => {
-    const selectTheme = localStorage.getItem('selected-theme');
-    localStorage.setItem('selected-theme', getCurrentTheme(selectTheme));
+    const selectTheme = localStorage.getItem(itemNameStorage);
+    localStorage.setItem(itemNameStorage, getCurrentTheme(selectTheme));
     if (darkThemeActive) {
       document.body.classList.add(darkTheme);
     } else {
@@ -36,27 +39,21 @@ const Header = () => {
     }
   }, [darkThemeActive]);
 
-  const linksNavitems = [
-    { link: '#home', title: 'Home', icon: foundIcon(icons, 'UilEstate', 'nav__icon') },
-    { link: '#about', title: 'About', icon: foundIcon(icons, 'UilUser', 'nav__icon') },
-    { link: '#skills', title: 'Skills', icon: foundIcon(icons, 'UilFileAlt', 'nav__icon') },
-    { link: '#services', title: 'Services', icon: foundIcon(icons, 'UilBriefcaseAlt', 'nav__icon') },
-    // { link: '#portfolio', title: 'Portfolio', icon: foundIcon(icons, 'UilScenery', 'nav__icon') },
-    // { link: '#contactme', title: 'Contactme', icon: foundIcon(icons, 'UilMessage', 'nav__icon') },
-  ];
+  const linksArr = [];
+  linksNavitems.forEach(linkInfo => linksArr.push({ link: linkInfo.link, title: linkInfo.title, icon: foundIcon(icons, linkInfo.iconName, linkInfo.iconClass) }));
 
   return (
     <header className="header scroll-header" id="header">
       <nav className="nav container">
-        <a href="!#" className="nav__logo">
-          Hugo
+        <a href={linkName} className="nav__logo">
+          {name}
         </a>
         <div className="nav__lang">
-          {langAvailables.map( (lang, index) => <div key={index} className={language === lang.toLocaleLowerCase() ? 'active-link' : ''} onClick={()=>changeLanguage(lang)}>{lang}</div>)}
+          {availablesLang.map( (lang, index) => <div key={index} className={language === lang.toLocaleLowerCase() ? 'active-link' : ''} onClick={()=>changeLanguage(lang)}>{lang}</div>)}
         </div>
         <div className={`nav__menu ${isActive ? "show-menu" : ""}`} id="nav-menu">
           <ul className="nav__list grid">
-            {linksNavitems.map((linkNavitem, index) => (
+            {linksArr.map((linkNavitem, index) => (
               <NavItems key={index} infoCard={linkNavitem} hideMenu={() => setActive(false)} />
             ))}
           </ul>
