bd4884ffdd0637b137daa2cf4e0a10040c3a0ea5
hugo cm <hugocmdesign@gmail.com>
2022-02-20 02:44:04 +0100

    split internal elements of Service section

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,50 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
 import '../assets/css/services.css';
 import customContext from '../context/customs/customsContext';
+import ServicesContent from './ui/ServicesContent';
 import foundIcon from './functions/foundIcon';
 import servicesInfo from './data/services';
-
-const ServicesList = ({ text, checkCircle }) => (
-  <li className="services__modal-service">
-    {checkCircle}
-    <p>{text}</p>
-  </li>
-);
-
-const ServicesContent = ({ cardInfo, clickerFn, iconsObj, textServicesContent }) => {
-  const {checkCircle, arrowRight, timesIcon } = iconsObj;
-  return (
-    <div className="services__content">
-      <div onClick={() => clickerFn(cardInfo.code)}>
-        {cardInfo.icon}
-        <h3 className="services__title">
-          {cardInfo.title}
-          <br />
-          <span className="services__subtitle">{cardInfo.subtitle}</span>
-        </h3>
-      </div>
-      <span className="button button--flex button--small button--link services__button" onClick={() => clickerFn(cardInfo.code)}>
-        {textServicesContent}
-        {arrowRight}
-      </span>
-      <div className={`services__modal ${cardInfo.isOpen && 'active-modal'}`} onClick={(e) => e.target.className === 'services__modal active-modal' && clickerFn(cardInfo.code)}>
-        <div className="services__modal-content">
-          <h4 className="services__modal-title">
-            {cardInfo.title}
-            <br />
-            <span className="services__subtitle">{cardInfo.subtitle}</span>
-          </h4>
-          <div onClick={() => clickerFn(cardInfo.code)} >{timesIcon}</div>
-          <ul className="services__modal-services grid">
-            {cardInfo.textsList.map((text, index) => (
-              <ServicesList key={index} text={text} checkCircle={checkCircle} />
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const Services = () => {
   const [servicesContent, setServicesContent] = useState([]);
---
+++ src/components/ui/ServicesContent.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const ServicesList = ({ text, checkCircle }) => (
+  <li className="services__modal-service">
+    {checkCircle}
+    <p>{text}</p>
+  </li>
+);
+
+const ServicesContent = ({ cardInfo, clickerFn, iconsObj, textServicesContent }) => {
+  const {checkCircle, arrowRight, timesIcon } = iconsObj;
+  return (
+    <div className="services__content">
+      <div onClick={() => clickerFn(cardInfo.code)}>
+        {cardInfo.icon}
+        <h3 className="services__title">
+          {cardInfo.title}
+          <br />
+          <span className="services__subtitle">{cardInfo.subtitle}</span>
+        </h3>
+      </div>
+      <span className="button button--flex button--small button--link services__button" onClick={() => clickerFn(cardInfo.code)}>
+        {textServicesContent}
+        {arrowRight}
+      </span>
+      <div className={`services__modal ${cardInfo.isOpen && 'active-modal'}`} onClick={(e) => e.target.className === 'services__modal active-modal' && clickerFn(cardInfo.code)}>
+        <div className="services__modal-content">
+          <h4 className="services__modal-title">
+            {cardInfo.title}
+            <br />
+            <span className="services__subtitle">{cardInfo.subtitle}</span>
+          </h4>
+          <div onClick={() => clickerFn(cardInfo.code)} >{timesIcon}</div>
+          <ul className="services__modal-services grid">
+            {cardInfo.textsList.map((text, index) => (
+              <ServicesList key={index} text={text} checkCircle={checkCircle} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesContent;
