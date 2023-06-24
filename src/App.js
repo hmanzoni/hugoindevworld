1137e1a5f7ba5e17e4c19cf28684daf0bf3ac9d2
hugo cm <hugocmdesign@gmail.com>
2023-06-25 01:09:00 +0200

    Fix roles switch

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Suspense, lazy } from 'react';
 import CustomsState from './context/customs/customsState';
 import Loader from './components/ui/Loader';
 import Layout from './components/Layout';
+import Home from './components/sections/Home';
+import About from './components/sections/About';
 
-const About = lazy(() => import('./components/sections/About'));
-const Home = lazy(() => import('./components/sections/Home'));
 const Skills = lazy(() => import('./components/sections/Skills'));
 const Services = lazy(() => import('./components/sections/Services'));
 const Qualification = lazy(() => import('./components/sections/Qualification'));
@@ -13,9 +13,9 @@ function App() {
   return (
     <CustomsState>
         <Layout>
+          <Home />
+          <About />
           <Suspense fallback={<Loader />}>
-              <Home />
-              <About />
               <Skills />
               <Qualification />
               <Services />
