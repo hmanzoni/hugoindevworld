29f96e040548de34fc49cc520b2d63782bf5dfe0
hmanzoni <hugocmdesign@gmail.com>
2026-02-07 12:11:49 +0100

    feat: App to accept and forward AppDependencies from main which call createDependencies().

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -4,14 +4,20 @@ import Loader from '@presentation/components/Loader';
 import Layout from '@presentation/components/Layout';
 import Home from '@presentation/sections/Home';
 import About from '@presentation/sections/About';
+import { AppDependencies } from './compositionRoot';
 
 const Skills = lazy(() => import('@presentation/sections/Skills'));
 const Services = lazy(() => import('@presentation/sections/Services'));
 const Qualification = lazy(() => import('@presentation/sections/Qualification'));
 
-function App() {
+function App({ contentRepo, iconProvider, storageProvider, browserProvider }: AppDependencies) {
   return (
-    <AppProvider>
+    <AppProvider
+      contentRepo={contentRepo}
+      iconProvider={iconProvider}
+      storageProvider={storageProvider}
+      browserProvider={browserProvider}
+    >
       <Layout>
         <Home />
         <About />
--- src/presentation/main.tsx
+++ src/presentation/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../index.css';
 import App from './App';
+import { createDependencies } from './compositionRoot';
+
+const dependencies = createDependencies();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App {...dependencies} />
   </React.StrictMode>,
   document.getElementById('root')
 );
